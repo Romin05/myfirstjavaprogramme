1a7b873c212f8d27ef564ba01b576702fe8f8e96
rominpatel <patelromin05@gmail.com>
2022-04-02 12:01:46 +0100

    manual testing topics

---
+++ ManualTestingTopics.java
@@ -0,0 +1,66 @@
+package com.manualtesting.java;
+
+public class ManualTestingTopics {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is Software");
+        System.out.println("System Software");
+        System.out.println("Application Software");
+        System.out.println("What is Software Testing");
+        System.out.println("What are the Benefits of Software Testing");
+        System.out.println("Who does Testing");
+        System.out.println("Qualification of Tester");
+        System.out.println("Challenges of Tester");
+        System.out.println("What is Manual Testing");
+        System.out.println("SDLC Software Development Life Cycle");
+        System.out.println("STLC Software Testing Life Cycle");
+        System.out.println("Principle of Software Testing");
+        System.out.println("Test Plan");
+        System.out.println("Test plan Template");
+        System.out.println("Test Scenario");
+        System.out.println("Test Scenario Template");
+        System.out.println("Test Case");
+        System.out.println("Test Case Template");
+        System.out.println("Required Traceability Matrix RTM");
+        System.out.println("Test Scenario VS Test Case");
+        System.out.println("Software Testing Techniques");
+        System.out.println("ECP Equivalence Class Partitioning");
+        System.out.println("BVA Boundary Value Analysis");
+        System.out.println("Error Guessing");
+        System.out.println("What is a Bug");
+        System.out.println("Bug Cycle");
+        System.out.println("Bug Template");
+        System.out.println("Severity Vs Priority");
+        System.out.println("What is Severity");
+        System.out.println("What is Priority");
+        System.out.println("What are Defect/Bug tracking tool");
+        System.out.println("What is Black Box Testing");
+        System.out.println("What is White Box Testing");
+        System.out.println("What is Functional Testing");
+        System.out.println("What is Non Functional Testing");
+        System.out.println("What is Confirmation Testing");
+        System.out.println("What is Maintenance Testing");
+        System.out.println("What is UAT User Acceptance Testing");
+        System.out.println("Static and Dynamic Testing");
+        System.out.println("What is Exploratory Testing");
+        System.out.println("What is Adhoc Testing");
+        System.out.println("Software Development Models");
+        System.out.println("Waterfall Model");
+        System.out.println("Iterative Model");
+        System.out.println("V Model");
+        System.out.println("Verification & Validation");
+        System.out.println("Agile Model");
+        System.out.println("Scrum Terminology");
+        System.out.println("Agile/Scrum Team");
+        System.out.println("Agile Meetings");
+        System.out.println("DoR Vs DoD");
+        System.out.println("TDD Test Driven Development");
+        System.out.println("BDD Behaviour Driven Development");
+        System.out.println("BDD Tools");
+        System.out.println("What is Gherkin");
+        System.out.println("What are BDD Keywords");
+        System.out.println("What is Maintenance");
+
+    }
+}
